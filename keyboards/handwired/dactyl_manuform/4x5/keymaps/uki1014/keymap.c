cbaa3af86d16df3ab2dd42a70d4610fa651583a9
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2022-03-05 18:53:26 +0900

    update keymap

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/uki1014/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/uki1014/keymap.c
@@ -1,14 +1,12 @@
+/*
+ * https://syon.github.io/refills/rid/1471100/
+ * https://github.com/qmk/qmk_firmware/blob/master/docs/keycodes.md
+ * */
 #include QMK_KEYBOARD_H
 
 #define _LAYER0 0
 #define _LAYER1 1
 #define _LAYER2 2
-#define _LAYER3 3
-
-#define SFT_ESC  SFT_T(KC_ESC)
-#define CTL_BSPC CTL_T(KC_BSPC)
-#define ALT_SPC  ALT_T(KC_SPC)
-#define SFT_ENT  SFT_T(KC_ENT)
 
 #define KC_ML KC_MS_LEFT
 #define KC_MR KC_MS_RIGHT
@@ -19,7 +17,6 @@
 
 #define LAYER1 MO(_LAYER1)
 #define LAYER2 MO(_LAYER2)
-#define LAYER3 MO(_LAYER3)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer0 (qwerty)
@@ -30,116 +27,88 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------|                             |------|------+------+------+------|
      * |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   '  |
      * |------+------+------+-------------,                             ,-------------+------+------+------,
-     *        |  [   | CTRL |                                                         |   -  |   =  |
-     *        '------+------'-------------'                             '-------------'------+------'
-     *                      | ESC  |  BS  |                             | SPACE|ENTER |
-     *                      |  +   |   +  |                             |  +   |  +   |
-     *                      | SHIFT| CTRL |                             | ALT  |SHIFT |
-     *                      '------+------'                             '------+------'
+     *        | CTRL |LAYER2|                                                         |LAYER1| CTRL |
+     *        '------+------'                                                         '------+------'
+     *                      |-----|-------|                             |-------|--------|
+     *                      |SHIFT| SPACE |                             | ENTER |   BS   |
+     *                      '-----+-------'                             '-------+--------'
      *                                    '------+------' '------+------'
-     *                                    | TAB  |  | | CTRL  | LANG2  |
+     *                                    |  TAB | CMD  | |LANG1 | CTRL |
      *                                    '------+------' '------+------'
-     *                                    | LAYER1|  ~   | | LAYER2  | y |
+     *                                    |      |      | | CTRL |      |
+     *                                    |  ALT |  NO  | |  +   | ALT  |
+     *                                    |      |      | | CMD  |      |
      *                                    '------+------' '------+------'
      */
     [_LAYER0] = LAYOUT(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT,
-                         	KC_LCTL, LAYER1,                                                        LAYER2, KC_LCTL,
-                                                     KC_LGUI, LSFT(KC_SPC), KC_ENT, LAYER3,
-                                                     KC_TAB,  KC_ESC,  KC_LNG1,  KC_LCTL,
-                                                     LAYER1,   KC_NO,   KC_Y, LAYER2
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT,
+                         	KC_LCTL, LAYER2,                                                               LAYER1, KC_LCTL,
+                                   KC_LSFT, KC_SPC,                                    KC_ENT,  KC_BSPC,
+                                                     KC_LGUI,  KC_NO, KC_LNG1, RCTL(KC_RGUI),
+                                                     KC_TAB, KC_LALT, KC_RCTL, KC_RALT
     ),
 
     /* Layer1
      * ,----------------------------------,                             ,----------------------------------,
-     * |      |      |  mup |      |      |                             | VOL+ |      |  up  |      | PgUp |
+     * |  1   |  2   |  3   |  *   |  +   |                             |  <   |  @   |  (   |  )   |  \   |
      * |------+------+------+------+------|                             |-------------+------+------+------|
-     * |      | mleft| mdown|mright|      |                             | MUTE | left | down |right | PgDn |
+     * |  4   |  5   |  6   |  /   |  -   |                             |  >   |  =   |  {   |  }   |  $   |
      * |------+------+------+------+------|                             |------|------+------+------+------|
-     * |      |      |      |      |      |                             | VOL- |  /   |  \   |  ?   |  |   |
+     * |  7   |  8   |  9   |  |   |  _   |                             |  ~   |  `   |  [   |  ]   |  !   |
      * |------+------+------+-------------,                             ,-------------+------+------+------,
-     *        |      |      |                                                         | mbtn |mbtn2 |
+     *        |      |      |                                                         |      |      |
      *        '------+------'-------------'                             '-------------'------+------'
      *                      |      |      |                             |      |      |
      *                      |      |      |                             |      |      |
      *                      |      |      |                             |      |      |
      *                      '------+------'                             '------+------'
      *                                    '------+------' '------+------'
-     *                                    |      |      | |      |      |
+     *                                    |      |      | | SPACE| TAB  |
      *                                    '------+------' '------+------'
      *                                    |      |      | |      |      |
      *                                    '------+------' '------+------'
      */
     [_LAYER1] = LAYOUT(
-        _______, _______, KC_MU,   _______, _______,                                      KC_VOLU, _______, KC_UP,   _______, KC_PGUP,
-        _______, KC_ML,   KC_MD,   KC_MR,   _______,                                      KC_MUTE, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,
-        _______, _______, _______, _______, _______,                                      KC_VOLD, KC_SLSH, KC_BSLS, KC_QUES, KC_PIPE,
-                 _______, _______,                                                                          KC_MB1,  KC_MB2,
-                                   _______, _______,                                      _______, _______,
-                                                     _______, _______,  _______, _______,
-                                                     _______, _______,  _______, _______
+        KC_1, KC_2, KC_3, KC_ASTR,  KC_PLUS,                                             KC_LABK, KC_AT,   KC_LPRN, KC_RPRN, KC_BSLS,
+        KC_4, KC_5, KC_6, KC_PSLS,  KC_PMNS,                                             KC_RABK, KC_PEQL, KC_LCBR, KC_RCBR, KC_DLR,
+        KC_7, KC_8, KC_9, KC_PIPE,  KC_UNDS,                                             KC_TILD, KC_GRV,  KC_LBRC, KC_RBRC, KC_EXLM,
+                 _______, _______,                                                                        _______,  _______,
+                                   _______, _______,                                     _______, _______,
+                                                     _______, _______,  KC_SPACE, _______,
+                                                     _______, _______,  KC_TAB,   _______
+
     ),
 
     /* Layer2
      * ,----------------------------------,                             ,----------------------------------,
-     * | F1   | F2   | F3   | F4   | F5   |                             |  F6  | F7   |  F8  |  F9  |  F10 |
+     * | F7   | F12  |  mup |mbtn2 | mbtn |                             |WRight| WUp  |  up  | WDown|WLeft |
      * |------+------+------+------+------|                             |-------------+------+------+------|
-     * |  1   |  2   |  3   |  4   |  5   |                             |  6   |  7   |  8   |  9   |  0   |
+     * | SHIFT| mleft| mdown|mright| CMD  |                             |  ^   | left | down |right |  $   |
      * |------+------+------+------+------|                             |------|------+------+------+------|
-     * |  !   |  @   |  #   |  $   |  %   |                             |  ^   |  &   |  *   |  (   |  )   |
+     * | Alt  | VOL- | VOL+ |Alt+SP| Alt  |                             | MUTE |  %   |  #   |  &   | TAB  |
      * |------+------+------+-------------,                             ,-------------+------+------+------,
-     *        | F11  | F12  |                                                         |  -   |  =   |
+     *        |      |      |                                                         |      |      |
      *        '------+------'-------------'                             '-------------'------+------'
-     *                      |      |      |                             |      |      |
-     *                      |      |      |                             |      |      |
-     *                      |      |      |                             |      |      |
+     *                      |      |      |                             | CTRL |      |
+     *                      |      |      |                             |  +   | ESC  |
+     *                      |      |      |                             | TAB  |      |
      *                      '------+------'                             '------+------'
      *                                    '------+------' '------+------'
-     *                                    |      |      | |      |      |
+     *                                    |      |      | | TAB  |SPACE |
      *                                    '------+------' '------+------'
-     *                                    |      |      | |      |      |
+     *                                    |      |      | | RESET|      |
      *                                    '------+------' '------+------'
      */
     [_LAYER2] = LAYOUT(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-                 KC_F11,  KC_F12,                                                                           _______, _______,
-                                   _______, _______,                                      _______, _______,
-                                                     _______, _______,  _______, _______,
-                                                     _______, _______,  _______, _______
-    ),
-
-    /* Layer3
-     * ,----------------------------------,                             ,----------------------------------,
-     * |      |      |  mup |      |      |                             | VOL+ |      |  up  |      | PgUp |
-     * |------+------+------+------+------|                             |-------------+------+------+------|
-     * |      | mleft| mdown|mright|      |                             | MUTE | left | down |right | PgDn |
-     * |------+------+------+------+------|                             |------|------+------+------+------|
-     * |      |      |      |      |      |                             | VOL- |  BS  |  \   |  ?   |  |   |
-     * |------+------+------+-------------,                             ,-------------+------+------+------,
-     *        |      |      |                                                         | mbtn |mbtn2 |
-     *        '------+------'-------------'                             '-------------'------+------'
-     *                      |      |      |                             |      |      |
-     *                      |      |      |                             |      |      |
-     *                      |      |      |                             |      |      |
-     *                      '------+------'                             '------+------'
-     *                                    '------+------' '------+------'
-     *                                    |      |      | |      |      |
-     *                                    '------+------' '------+------'
-     *                                    |      |      | |      |      |
-     *                                    '------+------' '------+------'
-     */
-    [_LAYER3] = LAYOUT(
-        _______, _______, KC_MU,   _______, _______,                                      KC_VOLU, _______, KC_UP,   _______, KC_PGUP,
-        _______, KC_ML,   KC_MD,   KC_MR,   _______,                                      KC_MUTE, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,
-        _______, _______, _______, _______, _______,                                      KC_VOLD, KC_BSPC, KC_BSLS, KC_QUES, KC_PIPE,
-                 _______, _______,                                                                          KC_MB1,  KC_MB2,
-                                   _______, _______,                                      _______, _______,
-                                                     _______, _______,  _______, _______,
-                                                     _______, RESET,  _______, _______
+        KC_F7,   KC_F12,  KC_MU,   KC_MB2,       KC_MB1,                             KC_WH_R, KC_WH_U, KC_UP,   KC_WH_D, KC_WH_L,
+        KC_LSFT, KC_ML,   KC_MD,   KC_MR,        KC_LGUI,                            KC_CIRC, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,
+        KC_LALT, KC_VOLD, KC_VOLU, LALT(KC_SPC), KC_LALT,                            KC_MUTE, KC_PERC, KC_HASH, KC_AMPR, KC_TAB,
+                 _______, _______,                                                                     _______, _______,
+                                   _______, _______,                                 RCTL(KC_TAB), KC_ESC,
+                                                 _______, _______,  KC_TAB,   RESET,
+                                                 _______, _______,  KC_SPACE, _______
     )
 };
 

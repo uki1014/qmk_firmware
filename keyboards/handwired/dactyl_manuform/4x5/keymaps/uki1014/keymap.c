73e2e31b84c8f5a98a49197d368740a7d2714dca
uki <yuki-shimizu@c-fo.com>
2022-03-06 14:47:36 +0900

    update keymap

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/uki1014/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/uki1014/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------|                             |------|------+------+------+------|
      * |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   '  |
      * |------+------+------+-------------,                             ,-------------+------+------+------,
-     *        | CTRL |LAYER2|                                                         |LAYER1| CTRL |
+     *        | CTRL |LAYER2|                                                         |LAYER1| SHIFT |
      *        '------+------'                                                         '------+------'
      *                      |-----|-------|                             |-------|--------|
      *                      |SHIFT| SPACE |                             | ENTER |   BS   |
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT,
-                         	KC_LCTL, LAYER2,                                                               LAYER1, KC_LCTL,
+                         	KC_LCTL, LAYER2,                                                               LAYER1,  KC_RSFT,
                                    KC_LSFT, KC_SPC,                                    KC_ENT,  KC_BSPC,
                                                      KC_LGUI,  KC_NO, KC_LNG1, RCTL(KC_RGUI),
                                                      KC_TAB, KC_LALT, KC_RCTL, KC_RALT

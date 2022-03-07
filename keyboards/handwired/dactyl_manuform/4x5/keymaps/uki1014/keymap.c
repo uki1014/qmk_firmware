c7137600069200c76df4d302c186812568b0541b
uki <yuki-shimizu@c-fo.com>
2022-03-07 15:34:14 +0900

    update keymap

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/uki1014/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/uki1014/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------|                             |------|------+------+------+------|
      * |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   '  |
      * |------+------+------+-------------,                             ,-------------+------+------+------,
-     *        | CTRL |LAYER2|                                                         |      | SHIFT|
+     *        | CTRL |LAYER2|                                                         |   /  | SHIFT|
      *        '------+------'                                                         '------+------'
      *                      |-----|-------|                             |-------|--------|
      *                      |SHIFT| SPACE |                             | ENTER | LAYER1 |
@@ -37,18 +37,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                                    '------+------' '------+------'
      *                                    |LAYER3| CMD  | |  BS  | TAB  |
      *                                    '------+------' '------+------'
-     *                                    |      |      | | CTRL |      |
-     *                                    |  ALT | TAB  | |  +   | CTRL |
-     *                                    |      |      | | CMD  |      |
+     *                                    |      | CTRL | |      |      |
+     *                                    |  ALT |  +   | | LANG1| CTRL |
+     *                                    |      | CMD  | |      |      |
      *                                    '------+------' '------+------'
      */
     [_LAYER0] = LAYOUT(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT,
-                         	KC_LCTL, LAYER2,                                                               _______, KC_RSFT,
+                         	KC_LCTL, LAYER2,                                                               KC_SLSH, KC_RSFT,
                                    KC_LSFT, KC_SPC,                                    KC_ENT,  LAYER1,
-                                                   KC_LGUI, KC_TAB,  KC_BSPC, RCTL(KC_RGUI), // + Qするやつ
+                                                   KC_LGUI, RCTL(KC_RGUI),   KC_BSPC,  KC_LNG1, // cmd ctrlは+ QしてPC閉じるやつ
                                                    LAYER3,  KC_LALT, KC_TAB, KC_RCTL
     ),
 
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, KC_BSLS, KC_ASTR,  KC_PLUS,                                     KC_LABK, KC_AT,   KC_LPRN, KC_RPRN, KC_AMPR,
         _______, KC_CIRC, KC_HASH, KC_SLSH,  KC_MINS,                                     KC_RABK, KC_PEQL, KC_LCBR, KC_RCBR, KC_DLR,
         _______, _______, KC_PERC, KC_PIPE,  KC_UNDS,                                     KC_TILD, KC_GRV,  KC_LBRC, KC_RBRC, KC_EXLM,
-                 _______, _______,                                                                          _______,  _______,
+                 _______, _______,                                                                          _______, _______,
                                    _______, _______,                                      _______, _______,
                                                      _______, _______,  _______, _______,
                                                      _______, _______,  _______, _______
@@ -89,13 +89,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------|                             |-------------+------+------+------|
      * | SHIFT| mleft| mdown|mright| CMD  |                             |  ^   | left | down |right |  $   |
      * |------+------+------+------+------|                             |------|------+------+------+------|
-     * | Alt  | F7   | F12  |Alt+SP| Alt  |                             |      | TAB  | mbtn | mbtn2|      |
+     * | Alt  | F7   | F12  |Alt+SP| Alt  |                             |      | TAB  | mbtn | mbtn2|CTR+TB|
      * |------+------+------+-------------,                             ,-------------+------+------+------,
      *        |      |      |                                                         |      |      |
      *        '------+------'-------------'                             '-------------'------+------'
-     *                      |      | CTRL |                             |      |      |
-     *                      |      |  +   |                             | LANG1| ESC  |
-     *                      |      | TAB  |                             |      |      |
+     *                      |      |      |                             |      |      |
+     *                      |      |      |                             |      | ESC  |
+     *                      |      |      |                             |      |      |
      *                      '------+------'                             '------+------'
      *                                    '------+------' '------+------'
      *                                    |      |      | | LANG2|      |
@@ -106,9 +106,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LAYER2] = LAYOUT(
         _______, _______,KC_MU,  _______,      _______,                                   KC_WH_R, KC_WH_U, KC_UP,   KC_WH_D, KC_WH_L,
         KC_LSFT, KC_ML,  KC_MD,  KC_MR,        KC_LGUI,                                   KC_CIRC, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,
-        KC_LALT, KC_F7,  KC_F12, LALT(KC_SPC), KC_LALT,                                   _______, KC_TAB,  KC_MB1, KC_MB2, _______,
-                 _______, _______,                                                                            _______, _______,
-                                   _______, RCTL(KC_TAB),                                   KC_LNG1, KC_ESC,
+        KC_LALT, KC_F7,  KC_F12, LALT(KC_SPC), KC_LALT,                                   _______, KC_TAB,  KC_MB1,  KC_MB2,  RCTL(KC_TAB),
+                 _______, _______,                                                                          _______, _______,
+                                 _______, _______,                                   _______, KC_ESC,
                                                          _______, _______,  KC_LNG2, RESET,
                                                          _______, _______,  _______, _______
     ),
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,----------------------------------,                             ,----------------------------------,
      * |      | VOL- | MUTE | VOL+ |      |                             |  1   |  2   |  3   |  4   |  5   |
      * |------+------+------+------+------|                             |-------------+------+------+------|
-     * |      | Prev | Stop | Next |      |                             |  6   |  7   |  8   |  9   |  0   |
+     * |      | Prev | Pause| Next |      |                             |  6   |  7   |  8   |  9   |  0   |
      * |------+------+------+------+------|                             |------|------+------+------+------|
      * |      |      |      |      |      |                             |      |      |      |      |      |
      * |------+------+------+-------------,                             ,-------------+------+------+------,
@@ -135,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_LAYER3] = LAYOUT(
         _______, KC_VOLD, KC_MUTE, KC_VOLU,  _______,                                    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
-        _______, KC_MPRV, KC_MSTP, KC_MNXT,  _______,                                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+        _______, KC_MPRV, KC_MPLY, KC_MNXT,  _______,                                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         _______, _______, _______, _______,  _______,                                    _______, _______, _______, _______, _______,
                  _______, _______,                                                                         _______, _______,
                                    _______, _______,                                     _______, _______,
